b97923a7cc100e88d1d9f611437d9b0f25bbb07f
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-03 13:34:04 -0500

    Using ToString()

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour {
 
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
     int guess;
 
     // Use this for initialization
@@ -16,8 +18,9 @@ public class NumberWizard : MonoBehaviour {
 	
     void StartGame()
     {
-        max = max + 1;
         guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;        
     }
     public void OnPressHigher()
     {
@@ -33,5 +36,6 @@ public class NumberWizard : MonoBehaviour {
     void NextGuess()
     {
         guess = (max + min) / 2;
+        guessText.text = guess.ToString();
     }
 }
