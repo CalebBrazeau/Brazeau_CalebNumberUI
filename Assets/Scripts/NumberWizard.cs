8eee4b203735e87f538a825c3d06912574ff5fd1
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-03 13:41:14 -0500

    Use Random.Range()

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -18,24 +18,22 @@ public class NumberWizard : MonoBehaviour {
 	
     void StartGame()
     {
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
-        max = max + 1;        
+        NextGuess();      
     }
     public void OnPressHigher()
     {
-        min = guess;
-        NextGuess();
 
+        min = guess + 1;
+        NextGuess();
     }
     public void OnPressLower()
     {
-        min = guess;
+        max = guess - 1;
         NextGuess();        
     }
     void NextGuess()
     {
-        guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
         guessText.text = guess.ToString();
     }
 }
